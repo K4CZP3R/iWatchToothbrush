261c3de32a978ecfb4cbff958d41592311f04de4
K4CZP3R <kacper@kacperserewis.net>
2021-07-14 14:14:14 +0200

    Initial Commit

---
+++ kacper WatchKit Extension/kacperApp.swift
@@ -0,0 +1,19 @@
+//
+//  kacperApp.swift
+//  kacper WatchKit Extension
+//
+//  Created by Kacper Serewiś on 14/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct kacperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
